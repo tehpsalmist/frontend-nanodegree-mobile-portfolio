26a2673b4d80993323fe85ca17cc558b74bdf5f9
Ben Steward <ronyfan102@gmail.com>
2017-11-09 21:13:05 -0500

    pizzaperf

--- views/js/main.js
+++ views/js/main.js
@@ -373,7 +373,6 @@ var pizzaElementGenerator = function(i) {
   pizzaDescriptionContainer = document.createElement("div");
 
   pizzaContainer.classList.add("randomPizzaContainer");
-  pizzaContainer.style.width = "33.33%";
   pizzaContainer.style.height = "325px";
   pizzaContainer.id = "pizza" + i;                // gives each pizza element a unique id
   pizzaImageContainer.style.width="35%";
