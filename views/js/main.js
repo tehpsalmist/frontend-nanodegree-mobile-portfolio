95f1508949eb29d228da13b7b05a0ebb53d8b842
Ben Steward <ronyfan102@gmail.com>
2017-11-09 20:15:12 -0500

    lol

--- views/js/main.js
+++ views/js/main.js
@@ -518,7 +518,7 @@ function updatePositions() {
 }
 
 // runs updatePositions on scroll
-window.addEventListener('scroll', updatePositions);
+//lol window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
